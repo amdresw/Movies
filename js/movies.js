838072ea3a1c8913d38fc35d78a3994e02550b7e
amdresw <amdresw@gmail.com>
2025-01-16 08:43:32 -0500

    feat: :sparkles: movies list
    js object for the movie list create

---
+++ js/movies.js
@@ -0,0 +1,191 @@
+export const movies = [
+    {
+        id: 1,
+        title: "Interstellar 5555",
+        image: "./images/5555.jpg",
+        summary: "Is a 2003 anime musical science fiction film. The film tells the story of the abduction and rescue of an extraterrestrial pop band and serves as a visual companion to Discovery, Daft Punk's second studio album. The film was produced by Toei Animation, directed by Kazuhisa Takenouchi and supervised by Leiji Matsumoto.The film has no dialogue, but instead consists of the entirety of Discovery with minimal sound effects.",
+        cast: ["Thomas Bangalter","Guy-Manuel de Homem-Christo"],
+        duration: "1h 5m",
+    },
+    
+    {
+        id: 2,
+        title: "Ready Player One",
+        image: "./images/RP1.jpg",
+        summary: "Ready Player One is a 2018 American science fiction action film based on Ernest Cline's novel of the same name. The film is set in 2045, where much of humanity uses the OASIS, a virtual reality simulation, to escape the real world. A teenage orphan finds clues to a contest that promises ownership of the OASIS to the winner, and he and his allies try to complete it before an evil corporation can do so.",
+        cast: ["Tye Sheridan","Olivia Cooke", "Mark Rylance", "Lena Waithe",],
+        duration: "2h 20m",
+    },
+    
+    {
+        id: 3,
+        title: "The Batman",
+        image: "./images/batman2.jpg",
+        summary: "The Batman is a 2022 American superhero film based on the DC Comics character. Directed by Matt Reeves, the film follows Bruce Wayne in his second year as Batman, investigating a series of murders orchestrated by the enigmatic Riddler, which lead him to uncover dark secrets about Gotham City and his own family.",
+        cast: ["Robert Pattinson", "Zoë Kravitz", "Paul Dano", "Jeffrey Wright", "John Turturro", "Colin Farrell"],
+        duration: "2h 55m",
+    },
+
+    {
+        id: 4,
+        title: "The Shining",
+        image: "./images/shiny.jpg",
+        summary: "The Shining is a 1980 psychological horror film directed by Stanley Kubrick and based on Stephen King's 1977 novel of the same name. The film follows Jack Torrance, an aspiring writer and recovering alcoholic, who takes a job as the winter caretaker of the isolated Overlook Hotel. As the harsh winter sets in, Jack descends into madness, threatening his wife and young son.",
+        cast: ["Jack Nicholson", "Shelley Duvall", "Danny Lloyd", "Scatman Crothers"],
+        duration: "2h 26m"
+    },
+    
+    {
+        id: 5, 
+        title: "Howl's Moving Castle",
+        image: "./images/howl.jpg",
+        summary: "Howl's Moving Castle is a 2004 Japanese animated fantasy film directed by Hayao Miyazaki and produced by Studio Ghibli. Based on the novel by Diana Wynne Jones, the story follows Sophie, a young woman cursed with an elderly appearance by a witch's spell, and her journey with the wizard Howl and his magical moving castle as they navigate a world of war and enchantments.",
+        cast: ["Chieko Baisho", "Takuya Kimura", "Akihiro Miwa", "Tatsuya Gashuin"],
+        duration: "1h 59m"
+    },
+      
+    {
+        id: 6,
+        title: "Django Unchained",
+        image: "./images/D.jango.jpg",
+        summary: "Django Unchained is a 2012 American revisionist Western film written and directed by Quentin Tarantino. The film follows Django, a freed slave, who partners with a bounty hunter, Dr. King Schultz, to rescue his wife from a ruthless plantation owner in the antebellum South.",
+        cast: ["Jamie Foxx", "Christoph Waltz", "Leonardo DiCaprio", "Kerry Washington", "Samuel L. Jackson"],
+        duration: "2h 45m"
+    },
+
+    {
+        id: 7,
+        title: "The Matrix",
+        image: "./images/matrix.jpg",
+        summary: "The Matrix is a 1999 science fiction action film written and directed by the Wachowskis. The film follows Neo, a computer hacker who discovers the truth about the simulated reality known as the Matrix and joins a rebellion against its creators to free humanity.",
+        cast: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss", "Hugo Weaving"],
+        duration: "2h 16m"
+    },
+
+    {
+        id: 8,
+        title: "Hereditary",
+        image: "./images/here.jpg",
+        summary: "Hereditary is a 2018 American supernatural horror film written and directed by Ari Aster. The film follows a family that begins to unravel after the death of their secretive matriarch, uncovering dark and disturbing secrets about their ancestry that lead to terrifying consequences.",
+        cast: ["Toni Collette", "Alex Wolff", "Milly Shapiro", "Gabriel Byrne"],
+        duration: "2h 7m"
+    },
+
+    {
+        id: 9,
+        title: "Your Name",
+        image: "./images/Ur name.jpg",
+        summary: "Your Name is a 2016 Japanese animated romantic fantasy drama film directed by Makoto Shinkai. The film follows two teenagers, Taki and Mitsuha, who mysteriously begin to swap bodies intermittently. As they develop a deep connection through their shared experiences, they must navigate a complex set of events that tie their fates together.",
+        cast: ["Ryunosuke Kamiki", "Mone Kamishiraishi", "Nobunaga Shimazaki", "Aoi Yûki"],
+        duration: "1h 46m"
+    },
+
+    {
+        id: 10,
+        title: "Blade Runner 2049",
+        image: "./images/blade.jpg",
+        summary: "Blade Runner 2049 is a 2017 American neo-noir science fiction film directed by Denis Villeneuve. A sequel to the 1982 film Blade Runner, the story takes place 30 years after the original, following a new blade runner, K, who uncovers a long-buried secret that could reshape society. His discovery leads him on a journey to find Rick Deckard, a former blade runner who has been missing for decades.",
+        cast: ["Ryan Gosling", "Harrison Ford", "Jared Leto", "Ana de Armas", "Robin Wright"],
+        duration: "2h 44m"
+    },
+
+    {
+        id: 11,
+        title: "The Conjuring",
+        image: "./images/conju.jpg",
+        summary: "The Conjuring is a 2013 American supernatural horror film directed by James Wan. Based on real events, the film follows paranormal investigators Ed and Lorraine Warren as they help a family terrorized by a dark presence in their farmhouse. As they delve deeper into the haunting, they face dangerous and malevolent forces.",
+        cast: ["Vera Farmiga", "Patrick Wilson", "Lili Taylor", "Ron Livingston"],
+        duration: "1h 52m"
+    },
+
+    {
+        id: 12,
+        title: "Ghost in the Shell",
+        image: "./images/Ghost.webp",
+        summary: "Ghost in the Shell is a 1995 Japanese animated science fiction film directed by Mamoru Oshii, based on the manga by Masamune Shirow. Set in a futuristic world where cybernetic enhancements are common, the story follows Major Motoko Kusanagi, a special ops officer in charge of hunting down a mysterious hacker known as the Puppet Master. As she investigates, she begins to question the nature of her own identity and consciousness.",
+        cast: ["Atsuko Tanaka", "Iemasa Kayumi", "Akio Ōtsuka", "Tesshō Genda"],
+        duration: "1h 23m"
+    },
+
+    {
+        id: 13,
+        title: "Gladiator",
+        image: "./images/gladiator.jpg",
+        summary: "Gladiator is a 2000 epic historical drama film directed by Ridley Scott. Set in ancient Rome, the film follows Maximus, a betrayed general who is forced into slavery and becomes a gladiator. As he seeks vengeance against Commodus, the corrupt emperor who murdered his family, Maximus rises to fame in the arena and plots his revenge.",
+        cast: ["Russell Crowe", "Joaquin Phoenix", "Connie Nielsen", "Oliver Reed", "Richard Harris"],
+        duration: "2h 35m"
+    },
+
+    {
+        id: 14,
+        title: "The Babadook",
+        image: "./images/Baba.jpg",
+        summary: "The Babadook is a 2014 Australian psychological horror film written and directed by Jennifer Kent. The story follows Amelia, a single mother struggling to raise her troubled son, Samuel, who becomes obsessed with a mysterious book called 'Mister Babadook'. As the strange entity from the book begins to manifest in their lives, Amelia must confront her darkest fears to protect her son and herself.",
+        cast: ["Essie Davis", "Noah Wiseman", "Daniel Henshall", "Barbara West"],
+        duration: "1h 33m"
+    },
+
+    {
+        id: 15,
+        title: "Spider-Man: Into the Spider-Verse",
+        image: "./images/spider-man.jpg",
+        summary: "Spider-Man: Into the Spider-Verse is a 2018 American animated superhero film directed by Bob Persichetti, Peter Ramsey, and Rodney Rothman. The film follows Miles Morales, a teenager who gains spider-like abilities and discovers a multiverse of Spider-People from different dimensions. Together, they must stop a powerful villain threatening the multiverse and learn what it truly means to be Spider-Man.",
+        cast: ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld", "Mahershala Ali", "Brian Tyree Henry"],
+        duration: "1h 57m"
+    },
+
+    {
+        id: 16,
+        title: "John Wick",
+        image: "./images/wick.jpg",
+        summary: "John Wick is a 2014 American action thriller film directed by Chad Stahelski. The film follows John Wick, a retired hitman seeking vengeance against those who killed his beloved dog, a final gift from his late wife. As he embarks on his relentless pursuit, he finds himself drawn back into the criminal underworld he had once left behind.",
+        cast: ["Keanu Reeves", "Michael Nyqvist", "Alfie Allen", "Willem Dafoe", "Adrianne Palicki"],
+        duration: "1h 41m"
+    },
+
+    {
+        id: 17,
+        title: "Tron: Legacy",
+        image: "./images/Tron.jpg",
+        summary: "Tron: Legacy is a 2010 American science fiction film directed by Joseph Kosinski and a sequel to the 1982 film Tron. The story follows Sam Flynn, the son of Kevin Flynn, who is transported into the digital world of the Grid. Alongside his father's creation, a mysterious program named Quorra, Sam must navigate a dangerous new world and face a powerful villain to save his father and escape the Grid.",
+        cast: ["Garrett Hedlund", "Jeff Bridges", "Olivia Wilde", "Bruce Boxleitner", "Michael Sheen"],
+        duration: "2h 6m"
+    },
+
+    {
+        id: 18,
+        title: "The Exorcist",
+        image: "./images/exo.jpg",
+        summary: "The Exorcist is a 1973 American supernatural horror film directed by William Friedkin, based on the novel by William Peter Blatty. The film follows a mother whose young daughter becomes mysteriously possessed by a demonic force. Desperate, she seeks the help of two priests who perform an exorcism to save her daughter from the malevolent spirit.",
+        cast: ["Ellen Burstyn", "Max von Sydow", "Linda Blair", "Jason Miller", "Lee J. Cobb"],
+        duration: "2h 2m"
+    },
+
+    {
+        id: 19,
+        title: "Mad Max: Fury Road",
+        image: "./images/mad_max_fury_road-459159396-large.jpg",
+        summary: "Mad Max: Fury Road is a 2015 Australian post-apocalyptic action film directed by George Miller. Set in a desert wasteland, the film follows Max, a lone wanderer, who teams up with Furiosa, a rebellious war captain, as they try to escape a tyrannical warlord. Together, they battle through a treacherous landscape while being pursued by the warlord's army.",
+        cast: ["Tom Hardy", "Charlize Theron", "Nicholas Hoult", "Hugh Keays-Byrne", "Zoë Kravitz"],
+        duration: "2h"
+    },
+
+    {
+        id: 20,
+        title: "Dune",
+        image: "./images/ddune.jpg",
+        summary: "Dune is a 2021 American epic science fiction film directed by Denis Villeneuve, based on the novel by Frank Herbert. The film follows Paul Atreides, a young nobleman who becomes embroiled in a deadly political struggle over the desert planet Arrakis, the only source of the universe's most valuable substance, spice. As Paul learns of his destiny, he must navigate treacherous forces and lead a rebellion to protect his family and the planet's inhabitants.",
+        cast: ["Timothée Chalamet", "Rebecca Ferguson", "Oscar Isaac", "Zendaya", "Josh Brolin"],
+        duration: "2h 35m"
+    },
+
+    {
+        id: 21,
+        title: "Don't Look Now",
+        image: "./images/look.jpg",
+        summary: "Don't Look Now is a 1973 British-Italian psychological horror film directed by Nicolas Roeg, based on a short story by Daphne du Maurier. The film follows a grieving couple, John and Laura, who are living in Venice after the death of their young daughter. As they cope with their loss, strange and unsettling events begin to unfold, leading John to question his own sanity and the supernatural forces around him.",
+        cast: ["Donald Sutherland", "Julie Christie", "Hilary Mason", "Clelia Matania"],
+        duration: "1h 50m"
+    },
+      
+];
